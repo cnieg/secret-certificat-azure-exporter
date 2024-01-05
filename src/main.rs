89c251602a0496540cc4f4e538ba08b40c621114
Gregory Cavelier <gregory.cavelier@cnieg.fr>
2024-01-05 17:30:00 +0100

    fix error messages

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn get_subscription_list() {
 #[tokio::main]
 async fn main() {
 
-    let tenant_id = env::var("TENANT_ID").expect("env variable TENANT_ID is not set. exiting.");
-    let client_id = env::var("CLIENT_ID").expect("env variable CLIENT_ID is not set. exiting.");
-    let client_secret = env::var("CLIENT_SECRET").expect("env variable CLIENT_SECRET is not set. exiting.");
-    let scope = env::var("SCOPE").expect("env variable SCOPE is not set. exiting.");
+    let tenant_id = env::var("TENANT_ID").expect("env variable TENANT_ID");
+    let client_id = env::var("CLIENT_ID").expect("env variable CLIENT_ID");
+    let client_secret = env::var("CLIENT_SECRET").expect("env variable CLIENT_SECRET");
+    let scope = env::var("SCOPE").expect("env variable SCOPE");
     let proxy = env::var("http_proxy").unwrap_or("http://proxy-http:8080".to_string());
 
     println!("Utilisation du proxy {proxy}");
