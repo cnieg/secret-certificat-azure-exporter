7ee7b7d34df721268367432669eba90d6ce692ca
Gregory 'GaLi' Cavelier <gcavelier@cleia.org>
2024-01-14 16:23:05 +0100

    returns prometheus metrics

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,159 @@
 use axum::{extract::State, http::StatusCode, routing::get, Router};
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
 use std::env;
-use std::sync::Arc;
 
 #[derive(Clone)]
 struct AppState {
-    tenant_id: Arc<String>,
-    client_id: Arc<String>,
-    client_secret: Arc<String>,
+    tenant_id: String,
+    client_id: String,
+    client_secret: String,
+    scope: String,
     http_client: reqwest::Client,
 }
 
-async fn get_subscription_list(state: AppState) -> Result<String, String> {
-    Ok("".to_string())
+#[derive(Deserialize, Debug)]
+struct Token {
+    access_token: String,
+    // TODO: get and handle the "expires_in" field
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct PasswordCredential {
+    end_date_time: DateTime<Utc>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct KeyCredential {
+    end_date_time: DateTime<Utc>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Application {
+    app_id: String,
+    display_name: String,
+    password_credentials: Vec<PasswordCredential>,
+    key_credentials: Vec<KeyCredential>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Applications {
+    value: Vec<Application>,
+}
+
+async fn get_token(state: &AppState) -> Result<Token, reqwest::Error> {
+    let params = [
+        ("client_id", state.client_id.to_string()),
+        ("scope", state.scope.to_string()),
+        ("client_secret", state.client_secret.to_string()),
+        ("grant_type", "client_credentials".to_string()),
+    ];
+    let res: Token = state
+        .http_client
+        .post(format!(
+            "https://login.microsoftonline.com/{}/oauth2/v2.0/token",
+            state.tenant_id
+        ))
+        .form(&params)
+        .send()
+        .await?
+        .error_for_status()?
+        .json()
+        .await?;
+
+    Ok(res)
+}
+
+async fn get_subscription_list(state: AppState) -> Result<String, reqwest::Error> {
+    let token = get_token(&state).await?;
+
+    let mut jours_restants: i64;
+    let date_now = Utc::now();
+
+    let applications: Applications = state
+        .http_client
+        .get("https://graph.microsoft.com/v1.0/applications")
+        .bearer_auth(&token.access_token)
+        .send()
+        .await?
+        .error_for_status()?
+        .json()
+        .await?;
+
+    let mut res = String::new(); // This is what we are going to return
+
+    for mut application in applications.value {
+        // On nettoie les noms pour remove les caractères spéciaux + espaces (pour la conversion en métriques)
+        application.app_id = application.app_id.replace('-', "_");
+        application.display_name = application
+            .display_name
+            .replace('-', "_")
+            .replace([' ', '(', ')'], "")
+            .replace(['é', 'ê', 'è', 'ë'], "e");
+
+        // Handle secrets
+        for (id, password_credential) in application.password_credentials.iter().enumerate() {
+            if date_now > password_credential.end_date_time {
+                println!(
+                    "Le secret n°{id} de l'application {} a expiré le {}",
+                    application.display_name,
+                    password_credential.end_date_time.format("%d/%m/%Y")
+                );
+                jours_restants = 0;
+            } else {
+                jours_restants = (password_credential.end_date_time - date_now).num_days();
+            }
+            res.push_str(&format!(
+                "# HELP application_{}_{id} Secret N°{id} pour l'application {}\n",
+                application.app_id, application.display_name
+            ));
+            res.push_str(&format!(
+                "# TYPE application_{}_{id} gauge\n",
+                application.app_id
+            ));
+            res.push_str(&format!(
+                "application_{}_{id}{{application=\"{}\",type=\"secret\",app=\"Azure Certificat Expiration\"}} {jours_restants}\n\n",
+                application.app_id,
+                application.display_name));
+        }
+
+        // Handle certificates
+        for (id, key_credential) in application.key_credentials.iter().enumerate() {
+            if date_now > key_credential.end_date_time {
+                println!(
+                    "Le certificat n°{id} de l'application {} a expiré le {}",
+                    application.display_name,
+                    key_credential.end_date_time.format("%d/%m/%Y")
+                );
+                jours_restants = 0;
+            } else {
+                jours_restants = (key_credential.end_date_time - date_now).num_days();
+            }
+            res.push_str(&format!(
+                "# HELP application_{}_{id} Certificat N°{id} pour l'application {}\n",
+                application.app_id, application.display_name
+            ));
+            res.push_str(&format!(
+                "# TYPE application_{}_{id} gauge\n",
+                application.app_id
+            ));
+            res.push_str(&format!(
+                "application_{}_{id}{{application=\"{}\",type=\"certificat\",app=\"Azure Certificat Expiration\"}} {jours_restants}\n\n",
+                application.app_id,
+                application.display_name));
+        }
+    }
+
+    Ok(res)
 }
 
 async fn get_subscription_list_handler(State(state): State<AppState>) -> (StatusCode, String) {
     match get_subscription_list(state).await {
         Ok(res) => (StatusCode::OK, res),
-        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
     }
 }
 
@@ -31,25 +167,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client_id = env::var("CLIENT_ID").expect("env variable CLIENT_ID");
     let client_secret = env::var("CLIENT_SECRET").expect("env variable CLIENT_SECRET");
     let scope = env::var("SCOPE").expect("env variable SCOPE");
-    let proxy = env::var("http_proxy").unwrap_or("http://proxy-http:8080".to_string());
+    let proxy_str = env::var("http_proxy").unwrap_or("http://proxy-http:8080".to_string());
 
-    println!("Utilisation du proxy {proxy}");
+    // Create an HTTP client with or without a proxy depending on the value of env::var("http_proxy")
+    let http_client = match proxy_str.as_str() {
+        "" => reqwest::Client::new(), // no proxy
+        _ => reqwest::ClientBuilder::new()
+            .proxy(reqwest::Proxy::https(&proxy_str)?)
+            .build()?,
+    };
+
+    println!("Utilisation du proxy '{proxy_str}'");
 
     println!(
         "tenant_id={tenant_id} client_id={client_id} client_secret={client_secret} scope={scope}"
     );
 
-    // Create a reqwest client
-    let client = reqwest::Client::new();
-
     let app = Router::new()
         .route("/", get(root_handler))
         .route("/metrics", get(get_subscription_list_handler))
         .with_state(AppState {
-            tenant_id: Arc::new(tenant_id),
-            client_id: Arc::new(client_id),
-            client_secret: Arc::new(client_secret),
-            http_client: reqwest::Client::new(),
+            tenant_id: tenant_id,
+            client_id: client_id,
+            client_secret: client_secret,
+            scope: scope,
+            http_client: http_client,
         });
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
